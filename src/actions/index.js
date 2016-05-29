a212d63634bfb11539f9db67ed88332c9f985290
Simon Burns <sburns77@gmail.com>
2016-05-29 18:07:24 -0500

    Fixing webpack

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,17 +4,19 @@ import parse from 'csv-parse';
 
 const apiKey = 'VJ6cBdw9ajZYok4srDmb';
 let csvData = [];
-const symbols = () => {
+// const symbols = () => {
 
-	fs.readFile('XTSE-datasets-codes.csv', function (err1, data) {
-    	if (err1) {
-        console.error(err1);
-    	} else {
-        var myData = JSON.parse(data);
-        //Do some operation on myData here
-    	}
-	}
-};
+// 	fs.readFile('XTSE-datasets-codes.csv', function (err1, data) {
+//     	if (err1) {
+//         console.error(err1);
+//     	} else {
+//     	console.log("this is the data", data);
+//         var myData = JSON.parse(data);
+//         //Do some operation on myData here
+//     	}
+// 	}
+// };
+// symbols();
 
 export default apiKey;
 export default csvData;
