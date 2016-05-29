d371597cdcebaabd31fcbcd8cfc61484e0584e56
Simon Burns <sburns77@gmail.com>
2016-05-29 17:57:31 -0500

    Added CSV file and fs readFile

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import files from 'fs';
+import parse from 'csv-parse';
 
 const apiKey = 'VJ6cBdw9ajZYok4srDmb';
+let csvData = [];
 const symbols = () => {
+	fs.createReadStream(req.file.path)
+	    .pipe(parse({delimiter: ':'}))
+	    .on('data', function(csvrow) {
+	        console.log(csvrow);
+	        //do something with csvrow
+	        csvData.push(csvrow);        
+	    })
+	    .on('end',function() {
+	      //do something wiht csvData
+	      console.log(csvData);
+	    });
 
-	
 };
 
 export default apiKey;
+export default csvData;
 
 
