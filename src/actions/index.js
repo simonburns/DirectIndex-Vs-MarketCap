22c2461258690210444b1a37f5c614a2f786f8ae
Simon Burns <sburns77@gmail.com>
2016-05-29 17:49:12 -0500

    setup

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const apiKey = 'VJ6cBdw9ajZYok4srDmb';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}
